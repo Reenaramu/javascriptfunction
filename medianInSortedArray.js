93b144eccf085b07e4bc9c741aa2a22cfb7b93e5
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:23:41 +0530

    Create medianInSortedArray.js

---
+++ medianInSortedArray.js
@@ -0,0 +1,26 @@
+//Return median of two sorted arrays of the same size--->>Anonymous function
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+
+
+
+inp.on("close", () => {
+
+
+
+
+
+
+});
