6756c31a708dcc25188ee4f849898e11d6b42cb2
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:17:11 +0530

    Update sumfunction.js

--- sumfunction.js
+++ sumfunction.js
@@ -1,1 +1,26 @@
+//Sum of all numbers in an array ---->>(Anonymous function)
 
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+const sum = (data = []) => {
+   var sum = 0;
+    for(var i = 0; i < data.length; i++) {
+        var number = Number(data[i]);
+        sum += number;
+    }
+    return sum; 
+}
+
+inp.on("close", () => {
+  var input = userInput[0].split(" ");
+    console.log(sum(input));
+  });
