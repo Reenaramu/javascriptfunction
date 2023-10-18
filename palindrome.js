7049ed74e59633f2286542ce80e13f2f383d9b37
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:22:16 +0530

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,26 @@
+  //Return all the palindromes in an array--->>Anonymous function
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+
+
+
+inp.on("close", () => {
+
+
+
+
+
+
+});
