22539e851f104377e5112ef5fa20f576982fec06
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 16:58:01 +0530

    Create Anonymousfunction.js

---
+++ Anonymousfunction.js
@@ -0,0 +1,31 @@
+//Print odd numbers in an array 
+// Anonymous function
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+const evenOdd = (data = []) => {
+    var output = [];
+      for (var i = 0; i < data.length; i++) {
+          var numbers = data[i].split(" ");                       
+          for (var j = 0; j < numbers.length; j++) {
+              var num = Number(numbers[j]);
+              if (num % 2 !== 0) {
+                  output.push(num);
+              }
+          }
+      }
+      console.log(output);
+}
+
+inp.on("close", () => {
+    evenOdd(userInput);
+      
+});
