b2ce33c427a5c138c2f654575566204a27561ac4
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:56:45 +0530

    Update IIFE2.js

--- IIFE2.js
+++ IIFE2.js
@@ -1,1 +1,46 @@
+//Convert all the strings to title caps in a string array----->>IIFE
 
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+const input = userInput[0] + ' ';
+  const output = [];
+  let temp = '';
+
+  (function () {
+    for (let i = 0; i < input.length; i++) {
+      if (input[i] !== ' ') {
+        temp += input[i];
+      } else {
+        if (temp !== '') {
+          output.push(temp);
+          temp = '';
+        }
+      }
+    }
+  })();
+
+  console.log((function (data) {
+    const result = [];
+    for (let i = 0; i < data.length; i++) {
+      const words = data[i].toLowerCase();
+      result.push(words.charAt(0).toUpperCase() + words.slice(1));
+    }
+    return result;
+  })(output));
+
+
+
+
+
+});
