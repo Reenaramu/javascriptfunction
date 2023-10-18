b2325d25d38641a76b469fb3402f48967d2f132c
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 18:07:59 +0530

    Update IIFE8.js

--- IIFE8.js
+++ IIFE8.js
@@ -1,1 +1,40 @@
+//Rotate an array by k times---->IIFE
 
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+inp.on("close", () => {
+(function() {
+  function rotateArr(data, k) {
+    const length = data.length;
+    k = k % length;
+
+    const rotatedArray = new Array(length);
+    for (let i = 0; i < length; i++) {
+      const newIndex = (i + k) % length;
+      rotatedArray[newIndex] = data[i];
+    }
+    return rotatedArray;
+  }
+
+  const input = userInput[0].split(" ");
+  const k = Number(userInput[1]);
+
+  for (let i = 0; i < input.length; i++) {
+    input[i] = Number(input[i]);
+  }
+
+  console.log(rotateArr(input, k));
+})();
+
+});
