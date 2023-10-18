f6b197bff4e0f55d5c693d6e03fee377e21e4114
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:31:13 +0530

    Update medianInSortedArray.js

--- medianInSortedArray.js
+++ medianInSortedArray.js
@@ -11,16 +11,34 @@ const userInput = [];
 inp.on("line", (data) => {
   userInput.push(data);
 });
+const findMedian = (data1 = [], data2 = []) => {
+  const length = data1.length + data2.length;
+  const middle = Math.floor(length / 2);
+  let i = 0, j = 0;
+  let previous, current;
+  
+  while (i + j <= middle) {
+    previous = current;
+    if (i === data1.length || (j < data2.length && Number(data2[j]) < Number(data1[i]))) {
+      current = Number(data2[j]);
+      j++;
+    } else {
+      current = Number(data1[i]);
+      i++;
+    }
+  }
 
-
-
-
+  if (length % 2 === 0) {
+    return (previous + current) / 2;
+  } else {
+    return current;
+  }
+}
 
 inp.on("close", () => {
-
-
-
-
-
+    const input1 = userInput[0].split(" ");
+    const input2 = userInput[1].split(" ");
+    
+    console.log(findMedian(input1, input2));
 
 });
