46b8bc8576a74a22737534b1fd5a72ea31f4ed21
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:42:51 +0530

    Update rotateArray.js

--- rotateArray.js
+++ rotateArray.js
@@ -11,16 +11,25 @@ const userInput = [];
 inp.on("line", (data) => {
   userInput.push(data);
 });
-
-
-
-
+const rotateArr = (data, k) => {
+    const length = data.length;
+    k = k % length;
+    
+    const rotatedArray = new Array(length);
+    for(let i = 0; i < length; i++) {
+        const newIndex = (i + k) % length;
+        rotatedArray[newIndex] = data[i];
+    }
+    return rotatedArray; 
+}
 
 inp.on("close", () => {
-
-
-
-
-
+var input = userInput[0].split(" ");
+    var k = Number(userInput[1]);
+    
+    for(let i = 0; i < input.length; i++) {
+        input[i] = Number(input[i]);
+    }
+    console.log(rotateArr(input, k));
 
 });
