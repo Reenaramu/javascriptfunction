c4c808e6802299fed4ae588833399a86821686a2
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 18:06:59 +0530

    Update IIFE7.js

--- IIFE7.js
+++ IIFE7.js
@@ -1,1 +1,44 @@
+//Remove duplicates from an array--->>IIFE
 
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+inp.on("close", () => {
+(function() {
+  const data = userInput[0].split(" ");
+  const input = [];
+  for (let i = 0; i < data.length; i++) {
+    input.push(Number(data[i]));
+  }
+
+  const removeDuplicates = function(data) {
+    const newArr = [];
+    for (let i = 0; i < data.length; i++) {
+      let isDuplicate = false;
+      for (let j = 0; j < newArr.length; j++) {
+        if (data[i] === newArr[j]) {
+          isDuplicate = true;
+          break;
+        }
+      }
+      if (!isDuplicate) {
+        newArr.push(data[i]);
+      }
+    }
+    return newArr;
+  };
+
+  console.log(removeDuplicates(input));
+})();
+
+});
