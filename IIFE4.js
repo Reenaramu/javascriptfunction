a44e69e42085a7522472de610dd6e615271627ac
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 18:01:00 +0530

    Update IIFE4.js

--- IIFE4.js
+++ IIFE4.js
@@ -1,1 +1,42 @@
+//Return all the prime numbers in an array---->>IIFE
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+inp.on("close", () => {
+const input = userInput[0].split(" ");
+  (function () {
+    function isPrime(number) {
+      if (number <= 1) {
+        return false;
+      }
+      for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    const output = [];
+    for (let i = 0; i < input.length; i++) {
+      const num = Number(input[i]);
+      if (isPrime(num)) {
+        output.push(num);
+      }
+    }
+    console.log(output);
+  })();
+
+});
 
