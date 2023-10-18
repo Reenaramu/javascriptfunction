c5dfb462d528f19b76886924e04b94e2f0835139
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-18 17:41:45 +0530

    Update removeDuplicatesInArray.js

--- removeDuplicatesInArray.js
+++ removeDuplicatesInArray.js
@@ -11,16 +11,29 @@ const userInput = [];
 inp.on("line", (data) => {
   userInput.push(data);
 });
-
-
-
-
+const removeDuplicates = (data = []) => {
+  const newArr = [];
+  for (let i = 0; i < data.length; i++) {
+    let isDuplicate = false;
+    for (let j = 0; j < newArr.length; j++) {
+      if (data[i] === newArr[j]) {
+        isDuplicate = true;
+        break;
+      }
+    }
+    if (!isDuplicate) {
+      newArr.push(data[i]);
+    }
+  }
+  return newArr;
+}
 
 inp.on("close", () => {
+const input = userInput[0].split(" ");
+  const output = [];
+  for (let i = 0; i < input.length; i++) {
+    output.push(Number(input[i]));
+  }
 
-
-
-
-
-
+  console.log(removeDuplicates(output));
 });
