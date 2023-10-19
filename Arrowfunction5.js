1d3438db622bc615a9fa00e28e2d71682c66fd62
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 10:12:53 +0530

    Create Arrowfunction5.js

---
+++ Arrowfunction5.js
@@ -0,0 +1,19 @@
+//e. Return all the palindromes in an array-------------->Arrow function
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+inp.on("close", () => {
+
+
+});
