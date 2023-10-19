6249b76c8c2bdf4db27a5c8753b8f28b599bf3a1
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 11:06:36 +0530

    Update Arrowfunction4.js

--- Arrowfunction4.js
+++ Arrowfunction4.js
@@ -12,8 +12,32 @@ inp.on("line", (data) => {
   userInput.push(data);
 });
 
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
 
 inp.on("close", () => {
+  
+    const input = userInput[0].split(" ");
+     const primeNumbers = (data = []) => {
+     const output = [];
+     for (let i = 0; i < data.length; i++) {
+      const num = Number(data[i]);
+      if (isPrime(num)) {
+        output.push(num);
+      }
+    }
+    return output;
+  };
 
-
+  console.log(primeNumbers(input));
+ 
 });
