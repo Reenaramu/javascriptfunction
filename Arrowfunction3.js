739e28a0f5b9118ebff264d641bd2564a8086483
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 10:08:40 +0530

    Create Arrowfunction3.js

---
+++ Arrowfunction3.js
@@ -0,0 +1,19 @@
+//c. Sum of all numbers in an array--------->Arrowfunction
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+inp.on("close", () => {
+
+
+});
