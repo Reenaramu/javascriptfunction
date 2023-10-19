94621f0f2257255fb63384c7f08706d63a856d41
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 11:07:13 +0530

    Update Arrowfunction5.js

--- Arrowfunction5.js
+++ Arrowfunction5.js
@@ -11,9 +11,32 @@ const userInput = [];
 inp.on("line", (data) => {
   userInput.push(data);
 });
-
+const isPalindrome = (str) => {
+    var output = '';
+        str = str.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+        for(var i = str.length-1; i >= 0; i--) {
+           output += str[i]; 
+        }
+        if(output === str) {
+            return true;
+        }
+        return false;
+}
 
 inp.on("close", () => {
+    
+    var input = userInput[0].split(" ");
+    const palindromes = (data = []) => {
+     var ans = [];
+     for(var j = 0; j < data.length; j++) {
+        if(isPalindrome(data[j])) {
+            ans.push(data[j]);
+        }
+     }
+    return ans;;
+  };
 
+  console.log(palindromes(input));
 
+ 
 });
