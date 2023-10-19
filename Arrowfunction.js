12c7f2f578a15b7ff30dce65c7412f583c8af3f2
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 10:07:30 +0530

    Create Arrowfunction.js

---
+++ Arrowfunction.js
@@ -0,0 +1,19 @@
+//b.  Convert all the strings to title caps in a string array----------->>Arrow function
+
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+
+inp.on("close", () => {
+
+
+});
