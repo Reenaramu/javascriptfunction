5c2b02a73f7d4b9d6233f2c301cac31e24170b7f
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-10-19 11:04:26 +0530

    Update Arrowfunction1.js

--- Arrowfunction1.js
+++ Arrowfunction1.js
@@ -1,4 +1,5 @@
 //a. Print odd numbers in an array---->>Arrow function
+
 const readline = require("readline");
 
 const inp = readline.createInterface({
@@ -11,8 +12,20 @@ inp.on("line", (data) => {
   userInput.push(data);
 });
 
+const evenOdd = (data = []) => {
+  var output = [];
+    for (var i = 0; i < data.length; i++) {
+        var numbers = data[i].split(" ");                       
+        for (var j = 0; j < numbers.length; j++) {
+            var num = Number(numbers[j]);
+            if (num % 2 !== 0) {
+                output.push(num);
+            }
+        }
+    }
+    console.log(output);
+}
 
 inp.on("close", () => {
-
-
+  evenOdd(userInput);
 });
